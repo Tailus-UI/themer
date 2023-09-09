0d7e9ef882781419c122cc82c5858a9cf18a6077
Meschac Irung <meschacirung@gmail.com>
2023-09-09 12:32:58 +0200

    [button] : create leading icon button variant

--- packages/components/button/src/index.ts
+++ packages/components/button/src/index.ts
@@ -1,4 +1,5 @@
 export { button } from "./themes/default/button";
 export { iconButton } from "./themes/default/iconButton";
+export { leadingIconButton } from "./themes/default/leadingIconButton";
 import button from "./themes/default/button";
 export default button;
