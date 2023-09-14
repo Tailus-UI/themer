cb71d135881791624d105901c1b4ec6cfa591a64
Meschac Irung <meschacirung@gmail.com>
2023-09-14 07:53:46 +0200

    [card] create gradient borders variant

--- packages/components/card/src/index.ts
+++ packages/components/card/src/index.ts
@@ -2,3 +2,4 @@ export { card } from "./themes/default/card";
 export { elevatedVariant } from "./themes/default/elevatedVariant";
 export { mixedVariant } from "./themes/default/mixedVariant";
 export { softVariant } from "./themes/default/softVariant";
+export { gradientBordersVariant } from "./themes/default/gradientBorders";
