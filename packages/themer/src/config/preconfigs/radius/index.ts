3d8038317bc349988d452de84309448d57dd8faf
Meschac Irung <meschacirung@gmail.com>
2023-10-01 09:17:24 +0200

    export border radius pre configs

---
+++ packages/themer/src/config/preconfigs/radius/index.ts
@@ -0,0 +1,12 @@
+import boxy from "./boxy";
+import sharp from "./sharp";
+import smooth from "./smooth";
+import smoothest from "./smoothest";
+
+export default {
+    boxy,
+    sharp,
+    smooth,
+    smoothest,
+};
+export { boxy, sharp, smooth, smoothest };
