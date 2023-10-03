0c2069b526f892bb4b097b3eab421770d4a65837
Meschac Irung <meschacirung@gmail.com>
2023-10-03 09:09:18 +0200

    docs [themer] : write configurations types docs

--- packages/themer/src/plugin/types.ts
+++ packages/themer/src/plugin/types.ts
@@ -1,15 +1,89 @@
 import { AccordionConfig, AlertConfig, AnnonceConfig } from "./../config/components";
 
 export type Config = {
+    /**
+     * The appearance of the theme.
+     * **Note:** Independant of tailwindcss `darkMode`. This props is used to generate the appearance of your theme.
+     * - `light` : Generate a light theme only.
+     * - `dark` : Generate a dark theme only.
+     * - `both` : Generate both light and dark themes.
+     * @default "both"
+     */
     appearance?: "light" | "dark" | "both";
+    /**
+     * The border radius of the the following components :
+     * - accordion
+     * - alert
+     * - annonce
+     * - button
+     * - card
+     * - flag
+     * - field
+     * - popover
+     * - tooltip
+     * - menu
+     * - switch
+     * - toast
+     * - tabs
+     * @default "smoothest"
+     */
     radius?: "boxy" | "sharp" | "smooth" | "smoothest";
+    /**
+     * The shadow of the following components :
+     * - accordion : elevated and outlined-elevated variants
+     * - alert
+     * - card
+     * - feedback : flag and toast components
+     * - menu : context menu, dropdown menu, select menu and popover components
+     */
     shadow?: {
+        /**
+         * The size of the shadow.
+         * **Note:** The `2xl` value is not supported, use `xxl` instead !.
+         * @default "xl"
+         */
         size: "none" | "default" | "sm" | "md" | "lg" | "xl" | "xxl";
+        /**
+         * The opacity of the shadow accepts value from `1` to `100`.
+         * @default 10
+         */
         opacity: number;
     };
-    border?: "lighter" | "light" | "dark" | "darker" | "darkest";
+    /**
+     * The border of the following components :
+     * - ui : accordion, annonce, card, separator and navigation menu components
+     * - feedback : alert-dialog, dialog, flag and toast components
+     * - menu : context menu, dropdown menu, select and popover components
+     * - tabs : tabs-list and tabs-indicator elements
+     * - field : soft variant
+     * - card : soft variant
+     *
+     * @default "light"
+     */
+    border?: "lighter" | "light" | "high" | "higher";
+    /**
+     * The background of the following components :
+     * - accordion : soft, ghost elevated(dark) variants
+     * - feedback : alert-dialog, dialog, flag components
+     * - menu : context menu, dropdown menu, and popover components
+     * - tabs : tabs-list and tabs-indicator elements
+     * - select
+     *
+     * @default "light"
+     */
     background?: "light" | "lighter" | "high" | "higher";
+    /**
+     * The padding of the following components :
+     * - card
+     * - flag
+     * - popover
+     *
+     * @default "larger"
+     */
     padding?: "small" | "medium" | "large" | "larger" | "largest";
+    /**
+     * Specific components configurations.
+     */
     components?: {
         /**
          * The config of the accordion component.
