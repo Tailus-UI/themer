8dc8b4eae1eaa840d19e40fb9a9c54f6faf4a9da
Meschac Irung <meschacirung@gmail.com>
2024-05-14 15:58:55 +0200

    refactor: Update theme values in components.ts and shade.ts

--- src/plugin/shade.ts
+++ src/plugin/shade.ts
@@ -1,40 +1,36 @@
 import plugin from "tailwindcss/plugin";
 
-const baseShades = {
-    "--ui-border-color": "theme('colors.gray.200')",
-    "--ui-bg": 'theme("colors.white/var(--ui-bg-opacity)")',
-    "--ui-soft-bg": "theme('colors.gray.100')",
-    "--input-bg": "theme('colors.gray.200')",
-    "--ui-bg-opacity": "1",
-    "--overlay-bg": "theme(colors.gray.950/0.25)",
-
-    "@apply  dark:[--overlay-bg:theme(colors.gray.950/0.5)] dark:[--input-bg:var(--ui-bg)] dark:[--ui-bg:theme(colors.gray.900)] dark:[--ui-fancy-bg:theme(colors.gray.950)]":
-        {},
-};
-
 export const shade = plugin(
     ({ addBase, theme, addComponents }) => {
         addBase({
+            ":root": {
+                "--ui-border-color": "theme('colors.gray.200')",
+                "--ui-bg": 'theme("colors.white/var(--ui-bg-opacity)")',
+                "--ui-soft-bg": "theme('colors.gray.100')",
+                "--input-bg": "theme('colors.gray.200')",
+                "--ui-bg-opacity": "1",
+                "--overlay-bg": "theme(colors.gray.950/0.25)",
+
+                "@apply  dark:[--overlay-bg:theme(colors.gray.950/0.5)] dark:[--input-bg:var(--ui-bg)] dark:[--ui-bg:theme(colors.gray.900)] dark:[--ui-fancy-bg:theme(colors.gray.950)]":
+                    {},
+            },
+
             '[data-shade="800"]': {
-                ...baseShades,
                 "@apply dark:[--ui-border-color:theme(colors.gray.700)] dark:[--ui-bg:theme(colors.gray.800)] dark:[--ui-soft-bg:theme(colors.gray.900)] dark:[--ui-fancy-bg:theme(colors.gray.925)] dark:[--ui-fancy-border-color:theme(colors.gray.700)]":
                     {},
             },
 
             '[data-shade="900"]': {
-                ...baseShades,
                 "@apply dark:[--ui-border-color:theme(colors.gray.800)] dark:[--ui-bg:theme(colors.gray.900)] dark:[--ui-soft-bg:theme(colors.gray.800)] dark:[--ui-fancy-border-color:theme(colors.gray.800)]":
                     {},
             },
 
             '[data-shade="925"]': {
-                ...baseShades,
                 "@apply dark:[--ui-border-color:theme(colors.gray.800)] dark:[--ui-bg:theme(colors.gray.925)] dark:[--ui-soft-bg:theme(colors.gray.800)] dark:[--ui-fancy-border-color:theme(colors.gray.800/0.75)]":
                     {},
             },
 
             '[data-shade="glassy"]': {
-                ...baseShades,
                 "--ui-bd-blur": theme("blur[2xl]"),
                 "--ui-bg-opacity": "0.75",
                 "--ui-bg": theme("colors.white/var(--ui-bg-opacity)"),
@@ -44,7 +40,6 @@ export const shade = plugin(
             },
 
             '[data-shade="950"]': {
-                ...baseShades,
                 "@apply dark:[--ui-border-color:theme(colors.gray.800)] dark:[--ui-bg:theme(colors.gray.950)] dark:[--ui-soft-bg:theme(colors.gray.800)] dark:[--ui-fancy-border-color:theme(colors.gray.500/0.15)]":
                     {},
             },
