2d8c894df1d1fcfeab3918bd8cc81e4f71181bd5
Meschac Irung <meschacirung@gmail.com>
2024-05-28 17:29:22 +0200

    update [switch] : adjust root border color

--- packages/themer/src/components/switch.ts
+++ packages/themer/src/components/switch.ts
@@ -1,26 +1,26 @@
 import { tv } from "tailwind-variants";
 export const switchTheme = tv({
     slots: {
-        root: "relative inline-block border-[0.5px] border group rounded-full h-5 w-8 bg-gray-300 dark:bg-white/15 outline-2 outline-primary-600 outline-offset-2 overflow-hidden before:absolute before:inset-0 before:rounded-full before:transition-[transform,background,shadow] before:duration-300 before:-translate-x-3 data-[state=checked]:before:-translate-x-0 focus-visible:outline data-[state=checked]:border-transparent disabled:bg-gray-100 dark:disabled:bg-gray-800 disabled:opacity-50 disabled:shadow-none disabled:data-[state=checked]:before:bg-gray-300 dark:disabled:data-[state=checked]:before:bg-gray-700 disabled:data-[state=checked]:before:shadow-none",
-        thumb: "absolute inset-x-[3px] inset-y-0 my-auto size-3.5 rounded-full bg-white shadow-sm shadow-gray-950/25 transition-[transform,width] ease-in-out duration-300 will-change-transform data-[state=checked]:translate-x-[11px]",
+        root: "relative inline-block border-[0.5px] border border-gray-950/5 group rounded-full h-5 w-8 bg-gray-200 transition dark:bg-white/15 outline-2 outline-primary-600 outline-offset-2 overflow-hidden focus-visible:outline disabled:bg-gray-100 dark:border-white/5 data-[state=checked]:border-white/5 dark:disabled:bg-gray-800 disabled:opacity-50 disabled:shadow-none",
+        thumb: "absolute inset-x-[1px] inset-y-0 my-auto size-4 rounded-full bg-white shadow-sm shadow-gray-950/25 transition-[transform,width] ease-in-out duration-300 will-change-transform data-[state=checked]:translate-x-3",
     },
     variants: {
         intent: {
             primary: {
-                root: "data-[state=checked]:before:bg-primary-600",
+                root: "data-[state=checked]:bg-primary-600",
             },
             secondary: {
-                root: "data-[state=checked]:before:bg-secondary-600",
+                root: "data-[state=checked]:bg-secondary-600",
             },
             accent: {
-                root: "data-[state=checked]:before:bg-accent-600",
+                root: "data-[state=checked]:bg-accent-600",
             },
             gray: {
-                root: "data-[state=checked]:before:bg-gray-600 dark:before:data-[state=checked]:bg-gray-400",
+                root: "data-[state=checked]:bg-gray-600 dark:data-[state=checked]:bg-gray-400",
                 thumb: "dark:data-[state=checked]:bg-gray-950",
             },
             neutral: {
-                root: "data-[state=checked]:before:bg-gray-950 dark:before:data-[state=checked]:bg-white",
+                root: "data-[state=checked]:bg-gray-950 dark:data-[state=checked]:bg-white",
                 thumb: "dark:bg-gray-950",
             },
         },
@@ -33,7 +33,8 @@ export const switchTheme = tv({
 export const fancySwitch = tv({
     extend: switchTheme,
     slots: {
-        root: "shadow-inner shadow-gray-950/5 dark:shadow-gray-950/50 before:inset-px before:transition-transform data-[state=checked]:[--radial-opacity:0.3] before:[box-shadow:rgba(255,255,255,0.1)_0px_1px_0px_0px_inset,var(--switch-border-color)_0px_0px_0px_1px] before:[background-image:radial-gradient(76%_151%_at_52%_-52%,rgba(255,255,255,var(--radial-opacity))_0%,transparent_100%)] disabled:before:[background-image:none]",
+        root: "shadow-inner shadow-gray-950/10 data-[state=checked]:border-none dark:shadow-gray-950/50 data-[state=checked]:[--radial-opacity:0.5] data-[state=checked]:[box-shadow:rgba(255,255,255,0.1)_0px_1px_0px_0px_inset,var(--switch-border-color)_0px_0px_0px_1px] data-[state=checked]:[background-image:radial-gradient(76%_151%_at_52%_-52%,rgba(255,255,255,var(--radial-opacity))_0%,transparent_100%)] disabled:[background-image:none]",
+        thumb: "data-[state=checked]:translate-x-3.5",
     },
     variants: {
         intent: {
