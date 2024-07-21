b5bda9bb1f682b744848774811f90d833b8eb2d9
Meschac Irung <meschacirung@gmail.com>
2024-07-21 18:11:35 +0200

    update tooltip styles

--- packages/themer/src/components/typography.ts
+++ packages/themer/src/components/typography.ts
@@ -300,8 +300,7 @@ export const codeTheme = tv({
                 "bg-secondary-50 text-secondary-600 dark:text-secondary-300 border-secondary-200 dark:border-secondary-500/20 dark:bg-secondary-500/5",
             accent: "bg-accent-50 text-accent-600 dark:text-accent-300 border-accent-200 dark:border-accent-500/20 dark:bg-accent-500/5",
             gray: "bg-gray-50 text-[--body-text-color] dark:border-gray-500/20 dark:bg-gray-500/5 dark:border-gray-500/20",
-            neutral:
-                "bg-gray-50 text-gray-950 dark:text-white dark:bg-gray-500/5 dark:border-gray-500/20",
+            neutral: "bg-gray-50 text-gray-950 dark:text-white dark:bg-gray-500/5 dark:border-gray-500/20",
         },
     },
     defaultVariants: {
