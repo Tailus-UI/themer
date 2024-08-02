2db713130ee4ce97851e6b480a76d1047d4354a2
Meschac Irung <meschacirung@gmail.com>
2024-08-02 16:19:18 +0200

    rewrite data visualizations props

--- packages/themer/src/components/visualizations/tooltip.ts
+++ packages/themer/src/components/visualizations/tooltip.ts
@@ -51,8 +51,8 @@ export const tooltip = tv({
 });
 
 export type TooltipProps = {
-    mixed: boolean;
-    fancy: boolean;
-    singleEntry: boolean;
-    indicator : keyof typeof tooltip.variants.indicator
+    mixed?: boolean;
+    fancy?: boolean;
+    singleEntry?: boolean;
+    indicator? : keyof typeof tooltip.variants.indicator
 }
